c1bd56aa167e11529e0bc1fa5c997159f07d3897
000xzhou <xzhou92@gmail.com>
2023-11-10 16:37:13 -0500

    add oop

---
+++ car-oo-project/car-oo.js
@@ -0,0 +1,77 @@
+class Vehicle {
+    constructor(a,b,c) {
+        this.make = a
+        this.model = b
+        this.year = c
+    }
+    honk() {
+        return 'Beep'
+    }
+    toString() {
+        return `The vehicle is a ${make} ${model} from ${year}`
+    }
+}
+
+class Car extends Vehicle {
+    constructor(a,b,c) {
+        super(a,b,c)
+        this.numWheels = 4
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(a,b,c) {
+        super(a,b,c)
+        this.numWheels = 2
+    }
+    revEngine() {
+        return 'VROOM!!!'
+    }
+}
+
+class Garage {
+    constructor(capacity ) {
+        this.capacity  = capacity 
+        this.vehicles  = []
+    }
+    add(vehicle) {
+        if (!(vehicle instanceof Vehicle)) return "Only vehicles are allowed in here!"
+        if(this.vehicles >= this.capacity) return "Sorry, we're full."
+        this.vehicles.push(vehicle)
+        return vehicle
+    }
+}
+
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+myFirstVehicle.honk(); // "Beep."
+myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
+
+
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+myFirstCar.honk();     // "Beep."
+myFirstCar.numWheels;  // 4
+
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+myFirstMotorcycle.toString();
+// "The vehicle is a Honda Nighthawk from 2000."
+
+myFirstMotorcycle.honk();     // "Beep."
+myFirstMotorcycle.revEngine(); // "VROOM!!!"
+myFirstMotorcycle.numWheels;  // 2
+
+
+let garage = new Garage(2);
+garage.vehicles; // []
+garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
+garage.vehicles; // [Car]
+garage.add("Taco"); // "Only vehicles are allowed in here!"
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
+// "Vehicle added!"
+garage.vehicles; // [Car, Motorcycle]
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
+// "Sorry, we're full."
